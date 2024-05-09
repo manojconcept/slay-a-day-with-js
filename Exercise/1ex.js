5af8b7c2140dae05c9355d48a62cbc05bbd1f095
manojconcept <manojveerapavu@gmail.com>
2024-05-09 14:48:40 +0530

    added exercise 1

---
+++ Exercise/1ex.js
@@ -0,0 +1,30 @@
+ let word = "pneumonoultramicroscopicsilicovolcanoconiosis";
+ /*
+ input : pneumonoultramicroscopicsilicovolcanoconiosis
+ output: p*******************************************s
+
+ step : 1
+ slice(start,end<length>) => slice(1,word.length) =>"neumonoultramicroscopicsilicovolcanoconiosi".length
+
+ step : 2
+ let firstWord = word[0].padEnd(word.slice(1,word.length).length,"*"); => p*******************************************
+ let secondWord = word[word.length-1]; //-> s
+ console.log(firstWord+secondWord); // => p*******************************************s
+
+
+ */
+
+
+// let inbetweenLength = word.slice(1,word.length).length;
+// let firstword = word[0];
+// let secondword = word[word.length-1].padStart(inbetweenLength,"*");
+// console.log(firstword+secondword);
+
+let firstWord = word[0].padEnd(word.slice(1,word.length).length,"*");
+let secondWord = word[word.length-1]; 
+console.log(firstWord+secondWord); 
+
+//----------------> loops
+//first = word[0] => p; padEnd(length,"what to come") => p**
+                    //
+//last = word[word.length-1] => s; => padStart(length, "what to come") => **i
