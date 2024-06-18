cb5c71a7a1d93d6946f8e2711a90b78fd2669f58
manojconcept <manojveerapavu@gmail.com>
2024-06-18 16:16:33 +0530

    update

--- Array/reduce.js
+++ Array/reduce.js
@@ -1,6 +1,6 @@
-let numbers = [1, 1, 1, 1, 1];
+let numbers = [1, 2, 3,4,5];
 
 // arry.reduce(callbackfunction(accumlator,current)=> accumlator + current,initial);
 
-let result = numbers.reduce((accumlator, current) => accumlator + current, 0);
+let result = numbers.reduce((accumlator,current)=>accumlator + current,100);
 console.log(result);
