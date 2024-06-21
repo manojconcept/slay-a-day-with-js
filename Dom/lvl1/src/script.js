df51a8b56ed8832746bc62dc694150cdd007fd34
manojconcept <manojveerapavu@gmail.com>
2024-06-21 16:56:25 +0530

    update

--- Dom/lvl1/src/script.js
+++ Dom/lvl1/src/script.js
@@ -1,7 +1,27 @@
 // console.log(document);
-console.log(document.head);
-console.log(document.body);
+const accessWholeDocument = document;
+const accessHead = document.head;
+const accessBody = document.body;
+//To Accessing the elements
+const accessId = document.getElementById("name");
+// console.log(accessName.innerText); // return an unique;
+const accessClass = document.getElementsByClassName("world-tag");
+// console.log(accessClass); //returns an array; --> access with index value and with innerText.
+const accessTagName = document.getElementsByTagName("div");
+// console.log(accessTagName); //retrun an array; --> access with index value and with innerText.
+const accessName = document.getElementsByName("color");
+// console.log(accessName); // retruns an array; --> access with index value and with innerText
+const accessOneAttributesandTagName = document.querySelector("#name"); //selecting first element with the id 
+// console.log(accessOneAttributesandTagName);
+const accessAllAttributesAndTagName = document.querySelectorAll("input");
+//console.log(accessAllAttributesAndTagName);  // retruns an array; --> access with index value and with innerText;
+// let chageName = prompt("say your age :");
 
-let accessName = document.body.getElementsByTagName("h1")
-console.log(accessName);
-accessName[accessName.length - 1].innerText = "restart";
+(function displayNames() {
+    let creatingElement = document.createElement("h1");
+    creatingElement.id = "box"
+    creatingElement.innerText = prompt("Enter your name");
+    return document.body.append(creatingElement);
+})();
+
+
